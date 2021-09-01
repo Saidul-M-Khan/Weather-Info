13f793e24109202aed85e0632c56e3b6a88e136b
Saidul-M-Khan <khan_saidul@yahoo.com>
2021-09-01 21:28:27 +0600

    7th commit

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ const API_KEY = `1ff6ccf1da1c9ea26bf26626b70bdca7`;
 const searchTemperature = () => {
     const city = document.getElementById('city-name').value;
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+    document.getElementById('city-name').value = '';
 
     fetch(url)
     .then(response => response.json())
@@ -15,8 +16,9 @@ const setInnerText = (id, info) => {
 
 const displayTemperature = (temperature) => {
     setInnerText('city', temperature.name);
-    setInnerText('temperature', temperature.main.temp);
+    setInnerText('temperature', `${temperature.main.temp}°C`);
     setInnerText('condition', temperature.weather[0].main);
+    setInnerText('country', `Country: ${temperature.sys.country}`);
     // set weather icon
     const url = `http://openweathermap.org/img/wn/${temperature.weather[0].icon}@2x.png`;
     const imgIcon = document.getElementById('weather-icon');
